131727144d71ce9ac36dcfaf9ea88d07d7802041
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-09-26 13:09:28 -0300

    importa componentes botão e input na aplicação

--- apps/alfabit/src/app/app.component.ts
+++ apps/alfabit/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ButtonComponent } from '@alfabit-alura/button';
+import { InputComponent } from '@alfabit-alura/input';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent, InputComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
